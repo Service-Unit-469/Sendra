a33a5b04dc0d4be26be7d161dc24226ce6ca5b26
Dan Klco <klco@adobe.com>
2026-01-23 10:07:48 -0500

    Trying to fix and optmize the deploy

--- packages/dashboard/e2e/util/config.ts
+++ packages/dashboard/e2e/util/config.ts
@@ -2,10 +2,21 @@ import { readFileSync, existsSync } from "fs";
 import z from "zod";
 
 export const getConfig = () => {
+
+    if(process.env.APP_URL && process.env.DATA_TABLE_NAME && process.env.RATE_LIMIT_TABLE_NAME) {
+        console.log("Using environment variables for config");
+        return {
+            domainName: process.env.APP_URL,
+            dataTableName: process.env.DATA_TABLE_NAME,
+            rateLimitTableName: process.env.RATE_LIMIT_TABLE_NAME,
+        }
+    }
+
     let outputsPath = ".sst/outputs.json";
     if (!existsSync(outputsPath)) {
         outputsPath = "../../.sst/outputs.json";
     }
+    console.log(`Using SST outputs for config from file: ${outputsPath}`);
     const outputs = JSON.parse(readFileSync(outputsPath, "utf8"));
 
     const outputSchema = z.object({
