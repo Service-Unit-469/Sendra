367c50bbc13ff87eac884a620735182438eaaf5b
Dan Klco <klco@adobe.com>
2026-01-24 17:55:21 -0500

    adusting settings to improve ci performance and removing mobile tests

--- packages/dashboard/playwright.config.ts
+++ packages/dashboard/playwright.config.ts
@@ -14,8 +14,7 @@ export default defineConfig({
   testDir: "./e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 1 : 0,
   reporter: "html",
   expect: {
     timeout: 10_000,
@@ -45,15 +44,5 @@ export default defineConfig({
       use: { ...devices["Desktop Safari"] },
       dependencies: ["create e2e user"],
     },
-    {
-      name: "Mobile Chrome",
-      use: { ...devices["Pixel 5"] },
-      dependencies: ["create e2e user"],
-    },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone 12"] },
-      dependencies: ["create e2e user"],
-    }
   ],
 });
